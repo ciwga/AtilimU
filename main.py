f1f41e5bf09e43e964209c5ceb48eef6edda137c
ciwga <m1.ciwga@gmail.com>
2024-01-29 22:48:18 +0300

    Download document option for moodle is added

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ import os
 import sys
 
 
+def clean_terminal():
+    if os.name == 'posix':
+        os.system('clear')
+    elif os.name == 'nt':
+        os.system('cls')
+    else:
+        pass
+
+
 def feedback():
     return_menu = 'Return to Menu (r): '
     q = input(return_menu)
     if q == 'r':
-        if os.name == 'posix':
-            os.system('clear')
-        os.system('cls')
+        clean_terminal()
         menu()
 
 
 def menu():
+    clean_terminal()
     tprint("ATILIM\nUNIVERSITY", font="small")
 
     print(
@@ -24,7 +32,8 @@ def menu():
         "4. Check the Opened Area Elective Courses\n",
         "5. Save Your Financial Pay Table\n",
         "6. Save Your KVKK Form\n",
-        "7. Exit"
+        "7. Download Moodle Main Course Page Documents\n",
+        "8. Exit"
     )
 
     client_dict = {
@@ -34,10 +43,23 @@ def menu():
         4: UniversityAcademicSystem().get_opened_area_elective_courses,
         5: Atacs().save_financial_pay_table,
         6: Atacs().save_kvkk_form,
-        7: sys.exit
+        7: AtilimMoodle().download_files_from_course_page,
+        8: sys.exit
     }
 
-    query = int(input(r'Choose One (1/2/3/4/5/6/7): '))
+    query = int(input(r'Choose One (1/2/3/4/5/6/7/8): '))
+    if query == 3:
+        save_all = input("Do you want to save all course announcements? (y/n): ")
+        if save_all.lower() == 'y':
+            AtilimMoodle().save_ann_messages(save_all=True)
+            feedback()
+        elif save_all.lower() == 'n':
+            AtilimMoodle().save_ann_messages()
+            feedback()
+        else:
+            print("Wrong input!")
+            feedback()
+
     client_dict[query]()
     feedback()
 
--- tools/moodle.py
+++ tools/moodle.py
@@ -50,6 +50,73 @@ class AtilimMoodle:
 
         return session, sesskey
 
+    def download_files_from_course_page(self, course_page_url: str = None):
+        if course_page_url is None:
+            course_page_url = input("Enter the Course Main Page URL: ")
+
+        session, temp = self.auth_moodle()
+        course_page_context = session.get(course_page_url)
+        soup = BeautifulSoup(course_page_context.content, "html.parser")
+
+        file_dict = {}
+
+        def dict_organizer(file_ext: str, name_value: str, url_value: str):
+            if file_ext in file_dict:
+                file_dict[file_ext].append({
+                    'file_name': name_value,
+                    'file_url': url_value
+                })
+            else:
+                file_dict[file_ext] = [{
+                    'file_name': name_value,
+                    'file_url': url_value
+                }]
+
+        activity_list = soup.find_all("div", attrs={"class": "activityinstance"})
+        for instance in activity_list:
+
+            img_tag = instance.find("img")
+            span_tag = instance.find("span", attrs={"class": "instancename"})
+            file_name = span_tag.text.strip()
+            if img_tag is not None:
+
+                file_url = instance.find("a")["href"]
+                file_icon_url = img_tag["src"]
+                if 'pdf' in file_icon_url:
+                    dict_organizer('pdf', file_name, file_url)
+                if 'spreadsheet' in file_icon_url:
+                    dict_organizer('xlsx', file_name, file_url)
+                if 'document' in file_icon_url:
+                    dict_organizer('docx', file_name, file_url)
+                if 'powerpoint' in file_icon_url:
+                    dict_organizer('pptx', file_name, file_url)
+                if 'text' in file_icon_url:
+                    dict_organizer('txt', file_name, file_url)
+                if 'archive' in file_icon_url:
+                    dict_organizer('zip', file_name, file_url)
+                if 'html' in file_icon_url:
+                    dict_organizer('html', file_name, file_url)
+
+        path_dir_name = course_page_url.split("=")[-1]
+        dir_path = Path(f"{self.path}/atilim_data/moodle/Course_Documents/{path_dir_name}")
+        dir_path.mkdir(parents=True, exist_ok=True)
+
+        time_status = tqdm(total=0, bar_format="{desc}", position=1)
+        for key, value in file_dict.items():
+            for index, _ in tqdm(enumerate(value), desc=f"Downloading {key} files", position=0, unit="file", total=len(value)):
+                file_path = Path(f"{dir_path}/{value[index]['file_name']}.{key}")
+                request_url = session.get(value[index]['file_url'])
+                if request_url.status_code == 200:
+                    with open(file_path, 'wb') as f:
+                        f.write(request_url.content)
+                else:
+                    tqdm.write(f"Error: {request_url.status_code}")
+
+                wait_time = random.uniform(1.0, 3.1)
+                info = f"Waiting for {wait_time:.2f} seconds before the next request..."
+                time_status.set_description_str(info)
+                time.sleep(wait_time)
+
     def taken_courses(self, user_id=None, check=False):
         service = f"{self.uri}/lib/ajax/service.php"
         session, sesskey = self.auth_moodle()
@@ -122,14 +189,15 @@ class AtilimMoodle:
                     urls, shortname = AtilimMoodle.retriever(data)
                     filename = Path(f"{self.path}/atilim_data/moodle/{urls[0].split('=')[1]}_{shortname}.html")
 
+            time_log = tqdm(total=0, bar_format="{desc}", position=1)
             with open(filename, "w", encoding="utf-8") as af:
                 for url in urls:
                     page = session.get(url)
                     s = BeautifulSoup(page.content, "html.parser")
 
                     wait_time = random.uniform(1.0, 2)
-                    info = str(f"Waiting for {wait_time:.2f} seconds before the next request...")
-                    tqdm.write(info, end="")
+                    info = f"Waiting for {wait_time:.2f} seconds before the next request..."
+                    time_log.set_description_str(info)
                     time.sleep(wait_time)
 
                     try:
@@ -148,11 +216,11 @@ class AtilimMoodle:
                                 anns = [x["href"] for x in forum]
 
                                 wait_time = random.uniform(1.0, 2.4)
-                                info = str(f"Waiting for {wait_time:.2f} seconds before the next request...")
-                                tqdm.write(info, end="")
+                                info = f"Waiting for {wait_time:.2f} seconds before the next request..."
+                                time_log.set_description_str(info)
                                 time.sleep(wait_time)
 
-                                for ann in tqdm(anns, desc='Announcements'):
+                                for ann in tqdm(anns, desc='Announcements', position=0):
                                     msg = session.get(ann)
                                     s3 = BeautifulSoup(msg.content, "html.parser")
                                     d = s3.find(
@@ -163,8 +231,8 @@ class AtilimMoodle:
                                     af.write("*" * 85)
 
                                     wait_time = random.uniform(1.0, 3.4)
-                                    info = str(f"\tWaiting for {wait_time:.2f} seconds before the next request...")
-                                    tqdm.write(info, end="")
+                                    info = f"Waiting for {wait_time:.2f} seconds before the next request..."
+                                    time_log.set_description_str(info)
                                     time.sleep(wait_time)
 
                     except AttributeError:
@@ -175,4 +243,4 @@ class AtilimMoodle:
 
 if __name__ == "__main__":
     moodle = AtilimMoodle()
-    moodle.save_ann_messages()
+    moodle.download_files_from_course_page()
